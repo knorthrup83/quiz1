d36b2f54b1116776c9deba1b31bbbd1ddcc8dc79
Kevin Northrup <knorthrup83@gmail.com>
2019-08-30 17:40:39 +0000

    Fixing migration issue

--- db/migrate/20190830130557_add_fields_to_recipes.rb
+++ db/migrate/20190830130557_add_fields_to_recipes.rb
@@ -1,6 +1,5 @@
 class AddFieldsToRecipes < ActiveRecord::Migration[5.2]
   def change
-  	add_column :recipes, :user_id, :integer
   	add_column :recipes, :title, :string
 	add_column :recipes, :description, :text
   end
