5a4d5eaefe9ad6a93ff5d15a673dc7e79b64a13f
knorthrup83@gmail.com <knorthrup83@gmail.com>
2019-08-30 17:52:02 +0000

    Still working on migration problem

--- db/migrate/20190830130557_add_fields_to_recipes.rb
+++ db/migrate/20190830130557_add_fields_to_recipes.rb
@@ -1,5 +1,6 @@
 class AddFieldsToRecipes < ActiveRecord::Migration[5.2]
   def change
+  	add_column :recipes, :user_id, :integer
   	add_column :recipes, :title, :string
 	add_column :recipes, :description, :text
   end
